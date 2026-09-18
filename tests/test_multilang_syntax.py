c402dceef05c43fe8ae48a5042fc2fc763956c4b
Elyes Hkiri <55228083+elyeshkiri@users.noreply.github.com>
2026-09-18 14:08:29 +0200

    test: use unified syntax dispatcher

--- tests/test_multilang_syntax.py
+++ tests/test_multilang_syntax.py
@@ -1,6 +1,6 @@
 from token_saver.repo_index import record_for_text
 from token_saver.snippet import extract_symbol
-from token_saver.syntax_multilang import symbols
+from token_saver.syntax import symbols
 
 
 def _qualified(text: str, suffix: str) -> set[str]:
